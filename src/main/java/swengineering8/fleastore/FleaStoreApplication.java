433503a6e7f94b42ec1c6eaf77e230cda5ab7f71
SeChang Jeong <ckd0325@gmail.com>
2022-11-05 11:52:17 +0900

    first commit

---
+++ src/main/java/swengineering8/fleastore/FleaStoreApplication.java
@@ -0,0 +1,13 @@
+package swengineering8.fleastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swengineering8/fleastore/FleaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package swengineering8.fleastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
